a7e61e50e8561a8899c872d35fd338600e80a7fd
Dalton Perkinson <dlperk@gmail.com>
2026-02-28 10:46:51 -0600

    Configure Vite for multiple HTML entry points

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                insights: resolve(__dirname, 'insights.html'),
+                whitepaper: resolve(__dirname, 'the-human-helm.html')
+            }
+        }
+    }
+});
