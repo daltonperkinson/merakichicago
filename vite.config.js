347d40fafbe2ea885be079ae376200217224a104
daltonperkinson <62475670+daltonperkinson@users.noreply.github.com>
2026-04-05 23:17:04 -0500

    Update vite.config.js
    HAI added

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import { resolve } from 'path';
 export default defineConfig({
     build: {
         rollupOptions: {
-            input: {
+           input: {
                 main: resolve(__dirname, 'index.html'),
                 insights: resolve(__dirname, 'insights.html'),
                 whitepaper: resolve(__dirname, 'the-human-helm.html'),
                 success: resolve(__dirname, 'success.html'),
                 wigglyville: resolve(__dirname, 'wigglyville/index.html'),
                 dashboard: resolve(__dirname, 'dashboard/index.html'),
-                roadmap: resolve(__dirname, 'roadmap/index.html')
+                roadmap: resolve(__dirname, 'roadmap/index.html'),
+                hai: resolve(__dirname, 'hai/index.html')
             }
         }
     }
